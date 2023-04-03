045a86ff1ac6e4d3206748474bd749a082689fa9
ak89mkii <ak89mkii@gmail.com>
2023-04-03 15:41:11 -0400

    Added skeleton code for undici HTTP requests.

--- discordbot.js
+++ discordbot.js
@@ -1,8 +1,10 @@
-// Initialize dotenv
+// Initialize dotenv.
 require('dotenv').config();
 
-// Discord.js versions ^13.0 require us to explicitly define client intents
+// Discord.js versions ^13.0 require us to explicitly define client intents.
 const { Client, EmbedBuilder, Events, GatewayIntentBits } = require('discord.js');
+// Imports undici library.
+const { request } = require('undici');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -10,13 +12,22 @@ client.once(Events.ClientReady, () => {
 	console.log('Ready!');
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+// client.on(Events.InteractionCreate, async interaction => {
+// 	if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
-	await interaction.deferReply();
+// 	const { commandName } = interaction;
+// 	await interaction.deferReply();
+// 	// ...
+// });
+
+client.on(Events.InteractionCreate, async interaction => {
 	// ...
+	if (commandName === 'cat') {
+		const catResult = await request('https://aws.random.cat/meow');
+		const { file } = await catResult.body.json();
+		interaction.editReply({ files: [file] });
+	}
 });
 
-// Log In our bot
+// Log In our bot.
 client.login('your-token-goes-here');
