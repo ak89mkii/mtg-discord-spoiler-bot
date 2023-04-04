f52e7bd0e5444dc6fcfb9841e4a5ff767d910b06
ak89mkii <ak89mkii@gmail.com>
2023-04-04 14:54:33 -0400

    Successfully fetches data from Scryfall api and initiates Discord bot reply.

--- README.md
+++ README.md
@@ -11,6 +11,8 @@
 ## Technologies Used:
 #### API:
 - Scryfall
+// https://scryfall.com/docs/syntax
+// https://scryfall.com/docs/api/cards/search
 #### Back-End:
 - Node.js
 #### Packages:
@@ -29,11 +31,12 @@
 #### Deployment:
 // 
 #### Depoyment Troubleshooting:
+// 
+#### Information References:
 // https://discordjs.guide/additional-info/rest-api.html#skeleton-code
 // https://discordjs.guide/slash-commands/response-methods.html#ephemeral-responses
 // https://discordjs.guide/additional-info/rest-api.html#making-http-requests-with-node
-#### Information References:
-// 
+// https://stackoverflow.com/questions/38884522/why-is-my-asynchronous-function-returning-promise-pending-instead-of-a-val
 #### Images References / Credits:
 #### Sounds References / Credits:
 #### Fonts / Credits:
--- commands/image.js
+++ commands/image.js
@@ -1,28 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { request } = require('undici');
+// const { request } = require('undici');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('newmtg')
 		.setDescription("Shows this year's (2023) MTG card spoilers."),
 	async execute(interaction) {
-	// 	const cardResult = await request('https://api.scryfall.com/cards/search?order=released&q=year=2023');
-	// 	const { file } = await cardResult.body.json();
-	// 	// await interaction.deferReply({ files: [file] });
-        
-    //     await console.log({ files: [file] });
-
-    //     function processData(responseData) {
-    //         // Here, you can put your code to process the data from response
-    //         console.log(responseData);
-    //     }
-        
-    //     fetch('https://api.scryfall.com/cards/search?order=released&q=year=2023')
-    //     .then(response => {
-    //         const data = response.json();
-        
-    //         processData(data);
-    //     });
 
         fetch("https://api.scryfall.com/cards/search?order=released&q=year=2023")
         .then(res => {
