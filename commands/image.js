0520a438d10f779cfa89d2fb1e11c3136643d26a
ak89mkii <ak89mkii@gmail.com>
2023-04-10 14:22:36 -0400

    Added 'cron' for automation. Fires, but throws error.

--- commands/image.js
+++ commands/image.js
@@ -1,32 +1,36 @@
 const { SlashCommandBuilder } = require('discord.js');
-// const { request } = require('undici');
+const cron = require("cron");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('newmtg')
 		.setDescription("Shows this year's (2023) MTG card spoilers."),
 	async execute(interaction) {
+        let initAutoSend01 = new cron.CronJob('00 15 02,14 * * *', () => {
 
-        fetch("https://api.scryfall.com/cards/search?order=released&q=year=2023")
-        .then(res => {
-            return res.json();
-        })
-        .then( async test => {
-            let dataSet = []
-            let dataImg = []
-            for(let i=10; i<20; i++) {
-                dataSet.push( '\n' + '**Card Set:** ' + '\n' + test.data[i].set_name)
-                console.log(dataSet)
-                dataSet.push( '\n' + '**Card Image Link:** ' + '\n' + test.data[i].image_uris.large)
+            fetch("https://api.scryfall.com/cards/search?order=released&q=year=2023")
+            .then(res => {
+                return res.json();
+            })
+            .then( async test => { 
+                let dataSet = []
+                let dataImg = []
+                for(let i=0; i<5; i++) {
+                    dataSet.push( '\n' + '**Card Set:** ' + '\n' + test.data[i].set_name)
+                    console.log(dataSet)
+                    dataSet.push( '\n' + '**Card Image Link:** ' + '\n' + test.data[i].image_uris.large)
 
-            }
-                console.log(test.data[10].set_name)
-            
+                }
+                console.log(test.data[5].set_name)
                 await interaction.reply(dataSet + '\n' + dataImg + '\n');
+            });
 
-                // await interaction.reply('**Card Set:** ' + '\n' + set + '\n' + test.data[i].image_uris.large);
-            // }
-           
+                    // await interaction.reply('**Card Set:** ' + '\n' + set + '\n' + test.data[i].image_uris.large);
+                // }
         });
+        initAutoSend01.start();
+        console.log("Initialized Automation.")
+
+
 	},
 };
