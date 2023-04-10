99f0d1e00f1d2fad5f78f721a1496b77cedb3d59
ak89mkii <ak89mkii@gmail.com>
2023-04-10 15:32:41 -0400

    Added 'cron' for automation. Fires, but throws error.

--- commands/image.js
+++ commands/image.js
@@ -6,31 +6,31 @@ module.exports = {
 		.setName('newmtg')
 		.setDescription("Shows this year's (2023) MTG card spoilers."),
 	async execute(interaction) {
-        let initAutoSend01 = new cron.CronJob('00 15 02,14 * * *', () => {
+        let initAutoSend01 = new cron.CronJob('00 32 02,15 * * *', () => {
 
             fetch("https://api.scryfall.com/cards/search?order=released&q=year=2023")
             .then(res => {
                 return res.json();
             })
-            .then( async test => { 
+            .then( async aPIData => { 
                 let dataSet = []
                 let dataImg = []
-                for(let i=0; i<5; i++) {
-                    dataSet.push( '\n' + '**Card Set:** ' + '\n' + test.data[i].set_name)
-                    console.log(dataSet)
-                    dataSet.push( '\n' + '**Card Image Link:** ' + '\n' + test.data[i].image_uris.large)
+                for(let i=0; i<2; i++) {
+                    dataSet.push( '\n' + '**Card Set:** ' + '\n' + aPIData.data[i].set_name)
+                    dataSet.push( '\n' + '**Card Image Link:** ' + '\n' + aPIData.data[i].image_uris.large)
 
                 }
-                console.log(test.data[5].set_name)
-                await interaction.reply(dataSet + '\n' + dataImg + '\n');
-            });
+                console.log(aPIData.data[0].set_name)
+                console.log(aPIData.data[0].set_name)
 
-                    // await interaction.reply('**Card Set:** ' + '\n' + set + '\n' + test.data[i].image_uris.large);
-                // }
+                // await interaction.reply(dataSet + '\n' + dataImg + '\n');
+                await interaction.reply("Test!");
+
+            });
         });
         initAutoSend01.start();
         console.log("Initialized Automation.")
-
+        // console.log(dataSet)
 
 	},
 };
